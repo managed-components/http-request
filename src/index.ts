f1c1bc540d8f52c2cc0c59109c3a70b10a30cdea
Yair Dovrat <yair@cloudflare.com>
2023-09-11 15:42:54 +0200

    consolidate get.ts, post.ts and index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,66 @@
-import { Manager } from '@managed-components/types'
-import { sendEventPost } from './post'
-import { sendEventGet } from './get'
+import { Manager, MCEvent } from '@managed-components/types'
+import { flattenKeys } from './utils'
 
+// Functions related to post requests
+
+function preparePostRequestBody(event: MCEvent) {
+  let requestBody = { ...event.payload }
+  delete requestBody.method
+  delete requestBody.endpoint //should we also cleanup "__zcl_track"?
+
+  // check if the request body has to be encoded
+  if (event.payload.method.endsWith('urlencoded')) {
+    requestBody = new URLSearchParams(flattenKeys(requestBody)).toString()
+  }
+  return requestBody
+}
+
+export function sendPostRequest(event: MCEvent) {
+  fetch(`${event.payload.endpoint}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(preparePostRequestBody(event)),
+  })
+}
+
+// Functions related to get requests
+function constructGetRequestUrl(event: MCEvent) {
+  const requestBody = { ...event.payload }
+  delete requestBody.method
+  delete requestBody.endpoint //should we also cleanup "__zcl_track"?
+  const url = new URL(`${event.payload.endpoint}`)
+  for (const [key, val] of new URLSearchParams(
+    flattenKeys(requestBody)
+  ).entries()) {
+    url.searchParams.append(key, val)
+  }
+  return url
+}
+
+export function sendGetRequest(event: MCEvent) {
+  fetch(`${constructGetRequestUrl(event)}`, {
+    method: 'GET',
+  })
+}
+
+// Event listeners
 export default async function (manager: Manager) {
   await manager.addEventListener('event', async event => {
-    console.log('Manager on event listener:', JSON.stringify(manager))
-    //checks if this is a POST or GET methos
+    console.log('Event Event:', JSON.stringify(event))
     if (event.payload.method.startsWith('post')) {
-      sendEventPost(event)
+      sendPostRequest(event)
     } else {
-      sendEventGet(event)
+      sendGetRequest(event)
     }
   })
   await manager.addEventListener('ecommerce', async event => {
-    console.log('Manager on ecom listener:', JSON.stringify(manager))
-    //checks if this is a POST or GET methos
+    console.log('Ecom Event:', JSON.stringify(event))
     if (event.payload.method.startsWith('post')) {
-      sendEventPost(event)
+      sendPostRequest(event)
     } else {
-      sendEventGet(event)
+      sendGetRequest(event)
     }
   })
 }
